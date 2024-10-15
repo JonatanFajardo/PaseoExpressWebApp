f938e548d89d5ffb8ab932e651ff6768805243be
Jonatan Fajardo <69583436+JonatanFajardo@users.noreply.github.com>
2024-10-14 22:11:16 -0700

    Formateo correcto de formulario y preparando nuevas paginas.

---
+++ PaseoExpressWebApp/Pages/ControlDiario.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PaseoExpressWebApp.Pages
+{
+    public class ControlDiarioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
