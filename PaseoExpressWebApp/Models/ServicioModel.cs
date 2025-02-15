55398a33c0cdaad55a3dad6665ed42a398c996cf
Jonatan Fajardo <69583436+JonatanFajardo@users.noreply.github.com>
2025-02-14 20:17:47 -0600

    Notificacion al guardar registro, validaciones etc

--- PaseoExpressWebApp/Models/ServicioModel.cs
+++ PaseoExpressWebApp/Models/ServicioModel.cs
@@ -15,6 +15,7 @@ namespace PaseoExpressWebApp.Models
         public string Titulo { get; set; }
 
         [StringLength(550)]
+        [Required(ErrorMessage = "El campo es obligatorio2.")]
         public string Descripcion { get; set; }
 
         public DateTime FechaServicio { get; set; }
@@ -26,37 +27,37 @@ namespace PaseoExpressWebApp.Models
         [Required(ErrorMessage = "El campo es obligatorio.")]
         public decimal CostoUnitario { get; set; }
 
-        [Range(0, 9999999999.99)]
+        [Range(1, 9999999999.99)]
         [Required(ErrorMessage = "El campo es obligatorio.")]
         public decimal CostoTotal { get; set; }
 
         [StringLength(250)]
         public string Marca { get; set; }
 
-        [Required(ErrorMessage = "El campo es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar el tipo de mantenimiento.")]
         public int IdTipoMantenimiento { get; set; }
 
-        public int KilometrajeVehiculo { get; set; }
 
         public int MillajeVehiculo { get; set; }
 
         public DateTime ProximaFechaMantenimiento { get; set; }
-         
+
+        [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar un vehículo.")]
         public int IdVehiculo { get; set; }
-         
+
         public int? IdUsuario { get; set; }
 
         public bool EsRecurrente { get; set; }
 
-        public long ProximoKilometraje { get; set; }
-
         public long ProximoMillaje { get; set; }
-         
+
+        //[Required(ErrorMessage = "Debe seleccionar una ubicación.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar una ubicación en el automóvil.")]
         public int IdUbicacionEnAutomovil { get; set; }
 
         [Range(0, 9999999999.99)]
         public decimal PrecioManoObra { get; set; }
-         
+
         public bool Confirmado { get; set; }
 
         [StringLength(250)]
